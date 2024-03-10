29df5b04de3de18123fb533b5f12a79e2ab9c818
Ignis-27 <muhammad.alam10@dcmail.ca>
2024-03-09 16:50:29 -0800

    string methods

--- Lab 4/Assignment 4 part 1/Main.js
+++ Lab 4/Assignment 4 part 1/Main.js
@@ -26,6 +26,12 @@ function result() {
     var yItem = randomValueFromArray(insertY);
     var zItem = randomValueFromArray(insertZ); 
 
+
+    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':inserty:',yItem);
+    newStory = newStory.replace(':insertz:',zItem);
+  
   if(customName.value !== '') {
     const name = customName.value;
 
