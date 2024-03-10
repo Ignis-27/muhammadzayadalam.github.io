ff178373ddb74b780edef5c360829ddb76501f99
Ignis-27 <muhammad.alam10@dcmail.ca>
2024-03-09 16:58:13 -0800

    adding comments

--- Lab 4/Assignment 4 part 1/Main.js
+++ Lab 4/Assignment 4 part 1/Main.js
@@ -6,8 +6,10 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+ 
+// craeting variables
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day." ;
 
 var insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
 
@@ -20,8 +22,11 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-    var newStory = storyText;
 
+    //string method 
+    var newStory = storyText;
+ 
+    //vraiables for thta go through the arrays 
     var xItem = randomValueFromArray(insertX);
     var yItem = randomValueFromArray(insertY);
     var zItem = randomValueFromArray(insertZ); 
@@ -45,7 +50,7 @@ function result() {
     newStory = newStory.replace('94 fahrenheit',temperature);
 
   }
-
+ //event handler 
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
