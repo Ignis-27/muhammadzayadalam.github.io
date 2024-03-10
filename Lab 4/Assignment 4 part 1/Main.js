8ff5ec5cbe8e2b6b9040f6e87b03033a64aa77c3
Ignis-27 <muhammad.alam10@dcmail.ca>
2024-03-09 16:48:30 -0800

    creating new variables for the result function

--- Lab 4/Assignment 4 part 1/Main.js
+++ Lab 4/Assignment 4 part 1/Main.js
@@ -11,7 +11,32 @@ var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. Whe
 
 var insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
 
-var insertY = ['the soup kitchen','Disneyland','the White House'];
+var insertY = ["the soup kitchen","Disneyland","the White House"];
 
 var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    var newStory = storyText;
+
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ); 
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
