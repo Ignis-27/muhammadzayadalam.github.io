617c266b59d199b8f6bbce96525d83dc06bf5386
Ignis-27 <muhammad.alam10@dcmail.ca>
2024-03-17 22:19:36 -0700

    wiring up the darken and lighten button

--- Lab 4/Assignment 4 part 2/gallery-start/main.js
+++ Lab 4/Assignment 4 part 2/gallery-start/main.js
@@ -31,6 +31,24 @@ for(let i = 0; i < images.length ; i++) {
         displayedImage.src = e.target.src;
         displayedImage.alt = e.target.alt;
     });
+
 }
 
+
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+    if(currentClass === 'dark'){
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+} else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+
+
+   }
+ 
+});
