227ebad52286149913c1247b670f17370cca98d7
Ignis-27 <muhammad.alam10@dcmail.ca>
2024-03-19 19:50:10 -0700

    making the checkbound() method

--- Lab 4/Assignment 4 part 4/main-finished.js
+++ Lab 4/Assignment 4 part 4/main-finished.js
@@ -112,6 +112,26 @@ class EvilCircle extends Shape{
       ctx.stroke();
     }
 
+    //check bound method 
+    checkBounds(); {
+      if (this.x + this.size >= width) {
+        this.x = this.size;
+      }
+  
+      if (this.x - this.size <= 0) {
+        this.x = this.size;
+      }
+  
+      if (this.y + this.size >= height) {
+        this.y = this.size;
+      }
+  
+      if (this.y - this.size <= 0) {
+        this.y = this.size;
+      }
+  
+     
+    }
 
   }
 }
